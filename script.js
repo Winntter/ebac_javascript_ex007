740bc0f30bf27b7c5ad5cd0c39eb650b680a0706
Witha <withackerfutema@hotmail.com>
2023-08-30 10:26:26 +0900

    Fix: Refactoring

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var submeterBtn = document.getElementById("submeterBtn");
-    var mensagem = document.getElementById("mensagem");
-  
-    submeterBtn.addEventListener("click", function() {
-        var campoA = parseFloat(document.getElementById("campoA").value);
-        var campoB = parseFloat(document.getElementById("campoB").value);
-  
-        if (campoB > campoA) {
-        mensagem.textContent = "Formulário válido! Número B é maior que número A.";
-        mensagem.style.color = "green";
-        } else {
-        mensagem.textContent = "Formulário inválido! Número B deve ser maior que número A.";
-        mensagem.style.color = "red";
-        }
-    });
-  });
+function validar() {
+  const msg = document.getElementById("msg");
+
+  const campoA = parseFloat(document.getElementById("campoA").value);
+  const campoB = parseFloat(document.getElementById("campoB").value);
+
+  if (campoB > campoA) {
+    msg.textContent = "Formulário válido! Número B é maior que número A.";
+    msg.style.color = "green";
+    } else {
+    msg.textContent ="Formulário inválido! Número B deve ser maior que número A.";
+    msg.style.color = "red";
+  }
+}
+
